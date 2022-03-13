b96a631b31eb4feef0725b90b0d3620c763a1fb7
Nehal Kankanawadi <nehalbk333@gmail.com>
2022-03-13 12:21:08 +0530

    Added Decryption Program

---
+++ decrypt.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 18:58:47 2022
+
+@author: Nehal Kankanawadi
+"""
+
+import cv2
+
+msg=[]
+
+print("Please place the encrypted image in current location as of this file with the name encrypted_image.png")
+
+#Input of Keys
+m=int(input("Enter first key : "))
+
+n=int(input("Enter second key : "))
+
+#Reading the image
+x=cv2.imread('encrypted_image.png')
+
+#Resizing to inital size
+x=cv2.resize(x,[m,n])
+
+#Converting ASCII value to chaareter
+for i in x:
+    for j in i:
+        msg.append(chr(int(j[0])))
+
+#Converting list to string
+msg=''.join(msg)
+
+print("\nYour scecret message :")
+print(msg)
